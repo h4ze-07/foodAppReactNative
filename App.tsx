8187c14ead10c358cee44f27fdb83b0a7333c855
Vladyslav <122152653+h4ze-07@users.noreply.github.com>
2024-07-01 17:01:09 +0300

    testing from other device

--- App.tsx
+++ App.tsx
@@ -1,13 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import { StatusBar, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import OnboardingScreen from './src/screens/OnboardingScreen'

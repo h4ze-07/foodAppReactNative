8cfbdf15f88c27f5b6911845b23ccdf746e201bb
Vladyslav <122152653+h4ze-07@users.noreply.github.com>
2024-07-02 16:27:42 +0300

    added asyncstorage to liked food

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
 import { TStoreItem } from "../types";
 import { foods } from "../constants";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 type LikedFood = TStoreItem[];
 
@@ -9,30 +10,54 @@ export class appStore {
     likedFood = <LikedFood>[];
 
     constructor() {
-        this.likedFood = [...foods];
         makeAutoObservable(this)
+        this.loadLikedFood()
     }
+
+    async loadLikedFood() {
+        try {
+            const likedFoodJson = await AsyncStorage.getItem('Liked');
+            if (likedFoodJson) {
+                const likedFoodArray = JSON.parse(likedFoodJson);
+                runInAction(() => {
+                    this.likedFood = likedFoodArray;
+                });
+            } else {
+                this.likedFood = [];
+            }
+        } catch (error) {
+            console.error("Failed to load liked food from AsyncStorage", error);
+        }
+    }
+
 
     get likedFoodLength() {
         return this.likedFood.length;
     }
 
-    get consoleFood() {
-        return this.likedFood
-    }
-
     addToLiked(item: TStoreItem) {
         this.likedFood.push(item);
+        this.updateStorage('Liked', JSON.stringify(this.likedFood))
     }
 
     removeFromLiked(name: string) {
         const newArr = this.likedFood.filter(i => i.name !== name)
         this.likedFood = newArr;
+        this.updateStorage('Liked', JSON.stringify(this.likedFood))
     }
 
     isItemInLikedFood(name: string) {
         return this.likedFood?.some(i => {
             return i.name === name;
         })
+    }
+
+
+    async updateStorage (key: string, data: string) {
+        try {
+            await AsyncStorage.setItem(key, data)
+        } catch (error) {
+            console.log(error);
+        }
     }
 }

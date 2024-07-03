751253bb183ebe95116cb28e83520e0238c0ddb5
Vladyslav <122152653+h4ze-07@users.noreply.github.com>
2024-07-03 16:45:16 +0300

    added UI for cart

---
+++ src/components/CartItem.tsx
@@ -0,0 +1,38 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import CustomButton from './CustomButton'
+import { TStoreItem } from '../types'
+
+const CartItem = ({ image, name, price, ingredients }: TStoreItem) => {
+    return (
+        <View className='mx-auto w-[95%] py-[10px] border border-slate-200 rounded-[20px] bg-mainWhite flex flex-row justify-between items-center px-[5px]'>
+            <View>
+                <Image source={image} className='w-[100] h-[100]' resizeMode='contain' />
+            </View>
+            <View className=''>
+                <Text className='font-bold text-mainDark text-[16px]'>{name}</Text>
+                <Text className='text-mainGrey text-[12px]'>{ingredients}</Text>
+                <Text className='font-bold text-mainDark text-[18px]'>${price}</Text>
+            </View>
+            <View className='items-center'>
+                <Text className='text-mainDark font-semibold text-[20px] mb-[10px]'>{3}</Text>
+                <View className='flex flex-row border border-primary rounded-[15px] overflow-hidden'>
+                    <CustomButton
+                        text='-'
+                        textStyles='font-bold text-mainWhite'
+                        containerStyles='bg-red-500 px-[20] py-[4]'
+                        onClick={console.log}
+                    />
+                    <CustomButton
+                        text='+'
+                        textStyles='font-bold text-mainWhite'
+                        containerStyles='bg-red-500 px-[20] py-[4]'
+                        onClick={console.log}
+                    />
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default CartItem
--- src/screens/tabs/CartTab.tsx
+++ src/screens/tabs/CartTab.tsx
@@ -1,11 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import CustomButton from '../../components/CustomButton'
+import { foods } from '../../constants'
+import CartItem from '../../components/CartItem'
 
 const CartTab = () => {
   return (
-    <SafeAreaView className='flex-1 bg-mainWhite p-[10px]'>
-      <Text>CartTab</Text>
+    <SafeAreaView className='flex-1 bg-mainWhite p-[5px]'>
+      {/* <ScrollView> */}
+      <FlatList
+        data={foods}
+        renderItem={({ item }) => <CartItem {...item} />}
+        keyExtractor={item => item.id}
+        contentContainerStyle={{ rowGap: 30 }}
+        showsVerticalScrollIndicator={false}
+      />
+      {/* </ScrollView> */}
+      <View className='pt-[40px] px-[5px]'>
+        <View className='flex flex-row justify-between'>
+          <Text className='text-mainDark font-bold text-[24px]'>Total price:</Text>
+          <Text className='text-mainDark font-bold text-[24px]'>${50}</Text>
+        </View>
+        <CustomButton
+          text='Checkout'
+          textStyles='text-mainWhite font-bold text-center text-[20px]'
+          containerStyles='bg-primary py-3 mt-[40px] mb-[50px] w-[70%] mx-auto rounded-[15px]'
+          onClick={console.log}
+        />
+      </View>
     </SafeAreaView>
   )
 }
